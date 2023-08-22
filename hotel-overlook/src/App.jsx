15273b338c676bd660ee0926a4bde6cfc5c61e7a
SlankehWeb <101549379+SlankehWeb@users.noreply.github.com>
2023-08-22 18:57:57 +0200

    footer 0.5
    most of the basic skeleton and some minor global chance to see the footer

--- hotel-overlook/src/App.jsx
+++ hotel-overlook/src/App.jsx
@@ -1,12 +1,14 @@
 import "./components/global/global.scss";
 import AppRouter from "./components/approuter/approuter";
 import NavBar from "./components/global/navbar/navbar";
+import GFooter from "./components/global/footer/footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <AppRouter />
+      <GFooter />
     </div>
   );
 }
